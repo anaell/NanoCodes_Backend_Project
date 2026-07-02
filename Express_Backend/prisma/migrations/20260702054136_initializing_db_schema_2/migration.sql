493e2a565b3877a4107976f6aeedd8d22348cfdd
anaell <chimereanaele@gmail.com>
2026-07-02 10:32:20 +0100

    feat(migration.sql): add comment and rating columns to Review table

---
+++ Express_Backend/prisma/migrations/20260702054136_initializing_db_schema_2/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `rating` to the `Review` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Review` ADD COLUMN `comment` VARCHAR(191) NULL,
+    ADD COLUMN `rating` INTEGER NOT NULL;
